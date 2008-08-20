9f914705b3c998b644bc2ff5cc4cbeeb331af10c
billdoor <billdoor@eec45573-e754-0410-a427-332ab9badd2b>
2008-08-20 18:28:06 +0000

    Initial source upload.

---
+++ trunk/VCTaxo.cpp
@@ -0,0 +1,24 @@
+#include "VCTaxo.h"
+
+VCTaxo g_vctaxo;
+
+VCTaxo::VCTaxo(void)
+{
+}
+
+VCTaxo::~VCTaxo(void)
+{
+}
+
+int VCTaxo::Init()
+{
+	return PLUGIN_OK;
+}
+
+void VCTaxo::Run(int arg)
+{
+}
+
+void VCTaxo::Term()
+{
+}
---
+++ trunk/VCTaxo.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <vector>
+#include <map>
+#include <algorithm>
+#include <string>
+#include <sstream>
+#include <boost/shared_ptr.hpp>
+
+#include <ida.hpp>
+#include <idp.hpp>
+#include <loader.hpp>
+#include <kernwin.hpp>
+#include <allins.hpp>
+#include <name.hpp>
+
+#include "VCClass.h"
+
+using namespace std;
+using namespace boost;
+
+class VCTaxo
+{
+public:
+	VCTaxo(void);
+	~VCTaxo(void);
+
+	int Init();
+	void Run(int arg);
+	void Term();
+
+private:
+	map<ea_t, shared_ptr<VCVMT>> _addr_to_vmt;
+};
+
+extern VCTaxo g_vctaxo;
